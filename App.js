58a31e307522cd44ba5352fd12a9047aff0b9590
Kraimps Pierre <pier_lupo@laposte.net>
2023-03-17 10:00:56 +0100

    init-tp-restau

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+
+function App(){
+  
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
