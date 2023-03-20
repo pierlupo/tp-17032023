8ffe2e3f7e7027255a4f8080bb78236cf2831ece
Kraimps Pierre <pier_lupo@laposte.net>
2023-03-20 21:38:07 +0100

    homepage catpage nav ok

--- component/MenuButton.js
+++ component/MenuButton.js
@@ -2,42 +2,55 @@ import {StyleSheet, Text, View, Pressable} from 'react-native';
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
 
-export default function MenuButton({
-  onPress,
-  title,
-  color,
-}) {
+export default function MenuButton({onPress, title, color}) {
   // const navigation = useNavigation();
   // function goToPage() {
   //   console.log('clic');
   //   navigation.navigate('MealsCat');
   // }
   return (
-    <Pressable onPress={onPress} style={({pressed}) =>  [ styles.button, pressed ? styles.buttonPressed : null] }>
-      <View style={[styles.catContainer, {backgroundColor: color}]}>
-        <Text style={styles.catTitle}>{title}</Text>
-      </View>
-    </Pressable>
+    <View style={styles.catContainer}>
+      <Pressable
+        onPress={onPress}
+        style={({pressed}) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}>
+        <View style={[styles.innerContainer, {backgroundColor: color}]}>
+          <Text style={styles.catTitle}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   catContainer: {
-    height: 175,
-    width: 175,
-    borderRadius: 5,
+    margin: 10,
+    flex:1,
+    height: 150,
+    width: 150,
+    borderRadius: 8,
+    elevation: 10,
+    backgroundColor: 'white',
   },
   catTitle: {
-    padding: 10,
-    fontSize: 22,
+    fontSize: Platform.OS === 'android' ? 20 : 30,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginTop: 60,
   },
-  button : {
-    flex : 1,
-},
-buttonPressed : {
-    opacity : 0.5,
-},
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 10,
+    borderRadius: 8,
+    elevation: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
